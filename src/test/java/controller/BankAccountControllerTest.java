2f29cfb42caca6ab0621a268c896c02bf73a84e3
Luigi Solitro <luigi.solitro@gmail.com>
2024-03-03 17:52:53 +0100

    Added config for Unit Test.

---
+++ src/test/java/controller/BankAccountControllerTest.java
@@ -0,0 +1,23 @@
+package controller;
+
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class BankAccountControllerTest {
+
+    Logger logger = LoggerFactory.getLogger(BankAccountControllerTest.class);
+
+
+    @Test
+    public void test_getBalance() {
+        logger.info("BankAccountControllerTest");
+     //   assertEquals("This is the testcase in this class", str1);
+    }
+
+}
